7e93ad2fbe089a6b901a136c2105899573b5f69d
iamamar11 <37834168+iamamar11@users.noreply.github.com>
2020-07-25 21:08:35 -0400

    html and file loader completed

--- WebpackLoaders/webpack.config.js
+++ WebpackLoaders/webpack.config.js
@@ -1,15 +1,17 @@
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
-var extractPlugin = new ExtractTextPlugin({
-    filename: "main.css"
-})
+// var extractPlugin = new ExtractTextPlugin({
+//     filename: "main.css"
+// })
 module.exports = {
     entry: "./src/js/app.js",
     output: {
         path: path.resolve(__dirname, 'dist'), // path of the dir
         filename: 'bundle.js',      // path of the filename name after comliling
-        publicPath: '/dist'
+        // publicPath: '/dist'
     },
     module:{
         rules: [
@@ -27,15 +29,39 @@ module.exports = {
             },
             {   // defining the rules for the plugin 
                 test: /\.scss$/,
-                use: extractPlugin.extract({
+                use: ExtractTextPlugin.extract({
                     use: ['css-loader','sass-loader']
                 })
+            },
+            {
+                test:/\.html$/,
+                loader: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
+                ]
             }
         ]
     },
     // to use the plugin we need the refrence here in plugin section even though we have used the plugin in loader section
     plugins: [
-        extractPlugin
-        // new ExtractTextPlugin("main.css")
+        new ExtractTextPlugin({
+            filename: "main.css"
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ['dist']
+        })
     ]
 }
